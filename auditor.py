a43f3e7e660961b88fc334a4c9f189d56898cad9
Jim <deeds08044@gmail.com>
2026-09-16 17:18:29 +0800

    Manage State: Keep a running total of the inventory

--- auditor.py
+++ auditor.py
@@ -1,6 +1,7 @@
-inventory ={
-  "Apple": 0
+inventory = {
+  "Apple": 0 
 }
+
 stop_prompt = False
 total_units_processed = 0
 failed_entries = 0
@@ -14,7 +15,8 @@ while stop_prompt == False:
     
     if stock_quantity.isdigit() and int(stock_quantity) >= 0:
         stock_quantity_int = int(stock_quantity)
-        inventory["Apple"] += stock_quantity_int
+        #For Step 6, "Manage State: Keep a running total of the inventory" is already fulfilled
+        inventory["Apple"] += stock_quantity_int 
         print(f"Current inventory: {stock_quantity_int}")
     else:
         print("An exception occured")    
