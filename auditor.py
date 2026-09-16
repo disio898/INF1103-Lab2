19bdb2f016ff354a85f637584143288e9e12b946
Jim <deeds08044@gmail.com>
2026-09-16 17:13:27 +0800

    If the user enters a string, reject it, print error, and move to the next iteration.

--- auditor.py
+++ auditor.py
@@ -1,12 +1,21 @@
 inventory ={
   "Apple": 0
 }
-
 stop_prompt = False
+total_units_processed = 0
+failed_entries = 0
+
 while stop_prompt == False:
 
     stock_quantity = input("Enter stock quantity (type quit to quit): ")
     if stock_quantity == "quit":
             stop_prompt = True
             break
-    inventory["Apple"] += stock_quantity_int
+    
+    if stock_quantity.isdigit():
+        stock_quantity_int = int(stock_quantity)
+        inventory["Apple"] += stock_quantity_int
+        print(f"Current inventory: {stock_quantity_int}")
+    else:
+        print("An exception occured")    
+  
