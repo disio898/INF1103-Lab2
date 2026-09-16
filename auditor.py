c2c49885f64752f32b906453230d9cd2ff5bdcab
Jim <deeds08044@gmail.com>
2026-09-16 17:20:23 +0800

    Trigger Overstock Alert: If the total inventory exceeds 500 units, print an alert and break the loop immediately.

--- auditor.py
+++ auditor.py
@@ -1,7 +1,6 @@
-inventory = {
-  "Apple": 0 
+inventory ={
+  "Apple": 0
 }
-
 stop_prompt = False
 total_units_processed = 0
 failed_entries = 0
@@ -11,13 +10,18 @@ while stop_prompt == False:
     stock_quantity = input("Enter stock quantity (type quit to quit): ")
     if stock_quantity == "quit":
             stop_prompt = True
+            print(f"Total units processed: {total_units_processed }")
             break
     
     if stock_quantity.isdigit() and int(stock_quantity) >= 0:
         stock_quantity_int = int(stock_quantity)
-        #For Step 6, "Manage State: Keep a running total of the inventory" is already fulfilled
-        inventory["Apple"] += stock_quantity_int 
-        print(f"Current inventory: {stock_quantity_int}")
+        if(inventory["Apple"] + stock_quantity_int <= 500):
+            inventory["Apple"] += stock_quantity_int
+            print(f"Current inventory: {stock_quantity_int}")
+        else:
+            print("Alert! Stock exceeds 500!")
+            break
     else:
         print("An exception occured")    
-  
+
+    
