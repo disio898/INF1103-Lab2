89a5e1427a3e86eaa0a01b3df1318b3bc1b4a60f
Jim <deeds08044@gmail.com>
2026-09-16 17:08:20 +0800

    Run in a continuous loop asking user to enter a stock quantity, until the user types quit

--- auditor.py
+++ auditor.py
@@ -1,3 +1,11 @@
 inventory ={
   "Apple": 0
 }
+
+stop_prompt = False
+while stop_prompt == False:
+
+    stock_quantity = input("Enter stock quantity (type quit to quit): ")
+    if stock_quantity == "quit":
+            stop_prompt = True
+            break
